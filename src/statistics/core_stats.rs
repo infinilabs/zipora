dd31aca390b9ffb560c803c3887e4f47bbe6e75e
Bin Wu <binwu@google.com>
2026-02-20 16:04:32 +0800

    chore: tidy up statistics

--- src/statistics/core_stats.rs
+++
@@ -1,9 +0,0 @@
-//! Core Statistics Types
-//!
-//! Provides the fundamental statistics types used throughout the framework.
-
-// Re-export from mod.rs
-pub use super::{
-    TrieStatistics, MemoryStats, PerformanceStats, CompressionStats,
-    DistributionStats, ErrorStats, TimingStats, MemoryCategory, ErrorType,
-};
